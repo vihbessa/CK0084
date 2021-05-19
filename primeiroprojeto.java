2d15942731eadcfc3c2218e1d2a9d62cf428c057
vihbessa <84298375+vihbessa@users.noreply.github.com>
2021-05-18 23:12:14 -0300

    Create primeiroprojeto.java

---
+++ primeiroprojeto.java
@@ -0,0 +1,22 @@
+
+/**
+ * Trabalho Prático.
+ * 
+ * @author (Vitória Bessa) 
+ * @version (2.0 18/05/2021)
+ */
+public class Projeto1
+{
+    // Declaracao de variaveis
+    private String saudacao;
+
+    /**
+     * Construtor para objetos da classe Projeto1
+     */
+    public Projeto1(String n)
+    {
+        saudacao = n;
+        System.out.print(saudacao);
+    }
+//Teste1
+}
